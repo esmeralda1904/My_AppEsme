9ca989b2543001c30992fda0118d4c475a0be466
Alumno 5 <Alumno5@iMac-de-UTZMG-7.local>
2025-09-25 08:34:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/my_appesmeralda/presentation/OtraVentana.kt
@@ -0,0 +1,11 @@
+package com.example.my_appesmeralda.presentation
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.my_appesmeralda.R
+
+class OtraVentana: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.otra_ventana)
+    }
+}
